3de4df91abee4ad932478945548262c2f43f4cf1
Alexandru Stan <alex@hypertriangle.com>
2014-06-08 05:29:15 +0100

    Working better, probably needs to be a kernel module to work properly

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
 #include "bitop.h"
 
 unsigned int reverse_bits(unsigned int input) {
-	const unsigned int bit_count = 9;
+	const unsigned int bit_count = 8;
 	unsigned int output=0;
 	for(unsigned char i=0;i<bit_count;i++) {
 		change_bit(output,(bit_count-1)-i,test_bit(input,i))
@@ -33,15 +33,14 @@ unsigned int reverse_bits(unsigned int input) {
 #define STOP_BIT 1
 
 // Writes an number of bytes to SPI
-void spi_uart_tx(char *tbuf, uint32_t len) {
-	bcm2835_gpio_fsel(RPI_GPIO_P1_19, BCM2835_GPIO_FSEL_OUTP); // MOSI
-	bcm2835_gpio_clr(RPI_GPIO_P1_19); //idle low
-	bcm2835_delayMicroseconds(4);
-	bcm2835_gpio_fsel(RPI_GPIO_P1_19, BCM2835_GPIO_FSEL_ALT0);
-	
+void spi_uart_tx(char c) {
 	volatile uint32_t* paddr = bcm2835_spi0 + BCM2835_SPI0_CS/4;
 	volatile uint32_t* fifo = bcm2835_spi0 + BCM2835_SPI0_FIFO/4;
-
+	
+	bcm2835_gpio_fsel(RPI_GPIO_P1_19, BCM2835_GPIO_FSEL_ALT0);
+	
+	//BUG: The start bit is always 1.5 periods long, probably unfixable
+	
 	// This is Polled transfer as per section 10.6.1
 	// BUG ALERT: what happens if we get interupted in this section, and someone else
 	// accesses a different peripheral?
@@ -49,40 +48,38 @@ void spi_uart_tx(char *tbuf, uint32_t len) {
 	// Clear TX and RX fifos
 	bcm2835_peri_set_bits(paddr, BCM2835_SPI0_CS_CLEAR, BCM2835_SPI0_CS_CLEAR);
 	
-	
-	bcm2835_peri_set_bits(paddr, BCM2835_SPI0_CS_LEN, BCM2835_SPI0_CS_LEN); //make it 9 bit
-	
-	
 	// Set TA = 1
 	bcm2835_peri_set_bits(paddr, BCM2835_SPI0_CS_TA, BCM2835_SPI0_CS_TA);
 	
-	uint32_t i;
-	for (i = 0; i < len; i++) {
 		// Maybe wait for TXD
 		while (!(bcm2835_peri_read(paddr) & BCM2835_SPI0_CS_TXD))
 			;
 		
 		// Write to FIFO, no barrier
-		bcm2835_peri_write_nb(fifo, reverse_bits(tbuf[i])+STOP_BIT);
+		bcm2835_peri_write_nb(fifo, reverse_bits(c));
 		
 		// Read from FIFO to prevent stalling
 		while (bcm2835_peri_read(paddr) & BCM2835_SPI0_CS_RXD)
 			(void) bcm2835_peri_read_nb(fifo);
-	}
+// 		bcm2835_gpio_fsel(RPI_GPIO_P1_19, BCM2835_GPIO_FSEL_ALT0);
 	
 	// Wait for DONE to be set
 	while (!(bcm2835_peri_read_nb(paddr) & BCM2835_SPI0_CS_DONE)) {
-		while (bcm2835_peri_read(paddr) & BCM2835_SPI0_CS_RXD)
-			(void) bcm2835_peri_read_nb(fifo);
+// 		while (bcm2835_peri_read(paddr) & BCM2835_SPI0_CS_RXD)
+// 			(void) bcm2835_peri_read_nb(fifo);
 	}
+	
+// 	bcm2835_delayMicroseconds(10);
 	
 	// Set TA = 0, and also set the barrier
 	bcm2835_peri_set_bits(paddr, 0, BCM2835_SPI0_CS_TA);
 	
-	bcm2835_spi_end();
+	//TODO: THe program might be interrupted in here, corrupting the character.
 	
 	bcm2835_gpio_fsel(RPI_GPIO_P1_19, BCM2835_GPIO_FSEL_OUTP); // MOSI
 	bcm2835_gpio_set(RPI_GPIO_P1_19); //idle high
+	bcm2835_gpio_set_pud(RPI_GPIO_P1_19,BCM2835_GPIO_PUD_UP);
+	bcm2835_delayMicroseconds(40);
 }
 
 int main(int argc, char **argv) {
@@ -90,26 +87,31 @@ int main(int argc, char **argv) {
 		return 1;
 	bcm2835_spi_begin();
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
-	bcm2835_spi_setDataMode(BCM2835_SPI_MODE3);
-	bcm2835_spi_setClockDivider(2170);
+	bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);
+	bcm2835_spi_setClockDivider(2170-200);
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
 	
-	unsigned char i='a';
+	volatile uint32_t* paddr = bcm2835_spi0 + BCM2835_SPI0_CS/4;
+	bcm2835_peri_set_bits(paddr, BCM2835_SPI0_CS_LEN, BCM2835_SPI0_CS_LEN); //make it 9 bit
+	
+	volatile uint32_t* ltoh = bcm2835_spi0+BCM2835_SPI0_LTOH/4;
+	bcm2835_peri_set_bits(ltoh,0b1111,0);
+	
+	unsigned int i='a';
 	
 	while(1) {
-		char buffer[] = {i++};
-		spi_uart_tx(buffer,sizeof(buffer));
+		for(char c='a';c<='z';c++) {
+			spi_uart_tx(c);
+			spi_uart_tx(' ');
+		}
+		spi_uart_tx('\n');
 		
-		if(i>'z')
-			i='\n';
-		
-		if(i==('\n'+1))
-			i='a';
-		
-		bcm2835_delay(100);
+// 		bcm2835_delay(10);
+// 		printf(" ");
 	}
 	
+	bcm2835_spi_end();
 	bcm2835_close();
 	
 	return 0;
